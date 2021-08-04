af314682da2cdf7620b9a2d378c29d8ec0c13053
Mikio Crosby <eremitik@Mikios-MacBook-Air.local>
2021-08-04 12:20:09 +0900

    to remove master

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+req
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -5,31 +6,23 @@ import store from "./store";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyC3itNzI3_a_quEBHK5pVfaC_YgFrXWSKI",
-    authDomain: "givesy-fda33.firebaseapp.com",
-    projectId: "givesy-fda33",
-    storageBucket: "givesy-fda33.appspot.com",
-    messagingSenderId: "494746875098",
-    appId: "1:494746875098:web:5c231b2304681bed2d21e3",
-    measurementId: "G-4RHCS2LK5M"
-  };
+console.log(process.env.VUE_APP_F_API_KEY)
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_F_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PID,
+  storageBucket: process.env.VUE_APP_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MSGID,
+  appId: process.env.VUE_APP_F_APP_ID,
+  measurementId: process.env.VUE_APP_MID
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(store).use(router).mount("#app");
 
 /*
-const firebaseConfig = {
-    apiKey: process.env.VUE_APP_F_API_KEY,
-    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-    databaseURL: process.env.VUE_APP_DATABASE_URL,
-    projectId: process.env.VUE_APP_PID,
-    storageBucket: process.env.VUE_APP_BUCKET,
-    messagingSenderId: process.env.VUE_APP_MSGID,
-    appId: process.env.VUE_APP_F_APP_ID,
-    measurementId: process.env.VUE_APP_MID
-  }
 
 firebase.initializeApp(firebaseConfig);
 
